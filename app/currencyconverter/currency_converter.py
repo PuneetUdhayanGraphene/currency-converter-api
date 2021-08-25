19031bdb1f6294d0a0a6caa4055843aeb76679ac
PuneetUdhayanGraphene <75603687+PuneetUdhayanGraphene@users.noreply.github.com>
2021-08-25 16:20:08 +0530

    database ORM added

--- app/currencyconverter/currency_converter.py
+++ app/currencyconverter/currency_converter.py
@@ -1,3 +1,10 @@
+from ..database import get_db
+from ..database.models import CurrencyDatabaseModel
+
+db = get_db()
+
+db.query(CurrencyDatabaseModel).get()
+
 def get_currencies():
     """Gagandeep"""
     pass
