d19c322eac30d0432b15490ff09d15f8eea8b01c
unknown <swapnilkumar@graphenesvc.com>
2021-09-08 17:24:25 +0530

    added docstrings

--- app/routers/currency.py
+++ app/routers/currency.py
@@ -6,9 +6,24 @@ router = APIRouter()
 
 @router.get('/currencies')
 def get_currencies():
+    """[Gets the currencies from database]
+
+    Returns:
+        [dictionary]: [Types of currencies available]
+    """
     return {"currencies": currency_converter.get_currencies()}
 
 @router.get('/getamount')
 def get_amount(amount : int, from_currency : str, to_currency: str):
+    """[Converts currencies from one form to another]
+
+    Args:
+        amount (int): [The amount value user wants to convert]
+        from_currency (str): [The currency user wants to convert from]
+        to_currency (str): [The currency user wants to convert to]
+
+    Returns:
+        [dictionary]: [The converted amount]
+    """
     amount = currency_converter.get_converted_amount(amount = amount, from_currency = from_currency, to_currency = to_currency)
     return {"converted_amount": amount}
